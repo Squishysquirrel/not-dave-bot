daf94435666f841da2bf97b605656d834a847a60
Squishysquirrel <Squishysquirrel9@gmail.com>
2019-04-18 23:32:13 -0400

    initial commit

---
+++ ex-config.py
@@ -0,0 +1,13 @@
+# rename to config.py to get the config file to import properly
+
+bot_token = 'MTgyMjg4MTA2MzgzMzQzNjE3.XLkxyg.UgPoJIhiEYqbWIIsmeqVZ59uVCc'
+# token for bot
+# format with ''
+
+user_to_ping = '245063734719021056'
+# id number of the discord user to ping
+# format with ''
+
+channel_to_send = 568564984184045588
+# format without ''
+# id number of the discord channel to ping in
---
+++ not-dave.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import asyncio
+import config
+
+bot = commands.Bot(command_prefix='?')
+
+async def annoy():
+	chan = bot.get_channel(config.channel_to_send)
+	await chan.send('<@!{}>'.format(config.user_to_ping))
+	await asyncio.sleep(5)
+
+@bot.event
+async def on_ready():
+    count = 0
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    while 1 == 1:
+        count = count + 1
+        if count%10 == 0:
+            print(count)
+        task = await asyncio.create_task(annoy())
+
+bot.run(config.bot_token)
